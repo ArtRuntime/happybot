75c72023d980be569cc22656fbfb341f46e719fe
ArtRuntime <alexbhaiya@duck.com>
2026-02-28 20:55:53 +0530

    huhu

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# echo "--- Starting Services ---"
-#
-# # 1. Start Wireproxy
-# echo "Starting Wireproxy..."
-# ./wireproxy -c proxy.conf &
-# sleep 2 # Give it a moment to start
-#
-# # 2. Start Bot and FastAPI
-# echo "Starting Telegram Bot..."
-# python3 -m bot &
-# echo "Starting Public API..."
-# uvicorn main:app --host 0.0.0.0 --port 7860
-
-
 pip install -U pip && \
 pip install -U -r requirements.txt > /dev/null 2>&1 && \
 pip install --upgrade py-tgcalls > /dev/null 2>&1 && \
@@ -29,3 +15,16 @@ python3 -m bot > /dev/null 2>&1 &
 echo "--- Starting Services ---"
 uvicorn main:app --host 0.0.0.0 --port 7860
 
+
+# echo "--- Starting Services ---"
+#
+# # 1. Start Wireproxy
+# echo "Starting Wireproxy..."
+# ./wireproxy -c proxy.conf &
+# sleep 2 # Give it a moment to start
+#
+# # 2. Start Bot and FastAPI
+# echo "Starting Telegram Bot..."
+# python3 -m bot &
+# echo "Starting Public API..."
+# uvicorn main:app --host 0.0.0.0 --port 7860
