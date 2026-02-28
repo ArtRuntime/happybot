292d6612b742ed34d5fe12317add2f654708a794
ArtRuntime <alexbhaiya@duck.com>
2026-02-28 05:39:54 +0530

    feat: Implement custom yt-dlp logging with verbosity control and improve message edit robustness by handling `MessageNotModified` errors.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
+# echo "--- Starting Services ---"
+#
+# # 1. Start Wireproxy
+# echo "Starting Wireproxy..."
+# ./wireproxy -c proxy.conf &
+# sleep 2 # Give it a moment to start
+#
+# # 2. Start Bot and FastAPI
+# echo "Starting Telegram Bot..."
+# python3 -m bot &
+# echo "Starting Public API..."
+# uvicorn main:app --host 0.0.0.0 --port 7860
+
+
+
 echo "--- Starting Services ---"
 
 # 1. Start Wireproxy
 echo "Starting Wireproxy..."
-./wireproxy -c proxy.conf &
+./wireproxy -c proxy.conf > /dev/null 2>&1 &
 sleep 2 # Give it a moment to start
 
 # 2. Start Bot and FastAPI
 echo "Starting Telegram Bot..."
-python3 -m bot &
+python3 -m bot > /dev/null 2>&1 &
 echo "Starting Public API..."
 uvicorn main:app --host 0.0.0.0 --port 7860
-
-
-
-
-
-
-
-# #!/bin/bash
-
-# echo "--- Starting Services ---"
-
-# # 1. Start Wireproxy
-# echo "Starting Wireproxy..."
-# ./wireproxy -c proxy.conf > /dev/null 2>&1 &
-# sleep 2 # Give it a moment to start
-
-# # 2. Start Bot and FastAPI
-# echo "Starting Telegram Bot..."
-# python3 -m bot > /dev/null 2>&1 &
-# echo "Starting Public API..."
-# uvicorn main:app --host 0.0.0.0 --port 7860
 
